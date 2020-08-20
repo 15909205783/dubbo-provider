84978ad2246f92d5b8325c3871646a3b66fe8acc
yangfan <2585928225@qq.com>
2020-08-20 23:09:24 +0800

    服务提供者

--- src/main/java/com/yangfan/service/App.java
+++ src/main/java/com/yangfan/service/App.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 public class App {
     public static void main(String[] args) throws IOException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"provider.xml"});
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"applicationContext.xml"});
         context.start();
         System.in.read(); // 按任意键退出
     }
